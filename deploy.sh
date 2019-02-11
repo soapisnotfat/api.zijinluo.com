de5a40fedb228f2263c1c3ff144c00adddd610e7
icpm <deovant@gmail.com>
2019-02-10 21:32:06 -0500

    update run and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 npm ci
 npm i pm2 typescript serve -g
-git submodule update --init --recursive
+git clone git@github.com:icpm/personal-data.git
 npm run build
 
 cp ../dotenv/api.zijinluo.com.env .env
 
-command="cd api.zijinluo.com && git pull && npm ci && git submodule update --remote --recursive && npm run build"
+command="cd api.zijinluo.com && git pull && npm ci && cd personal-data && git pull && cd .. && git  && npm run build"
 job="0 0 * * * $command"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-npm ci
+npm i
+git clone git@github.com:icpm/personal-data.git
